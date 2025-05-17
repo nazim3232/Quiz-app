4f5f0454288edfefb23ba52829565a70a1383dea
nazim3232 <chowdhurynazim32@gmail.com>
2025-05-17 23:52:19 +0600

    The project is finished but with a problem .

--- script.js
+++ script.js
@@ -39,6 +39,9 @@ correct : 2
 let questions = [...quizData].sort(()=>Math.random()-0.5);
 let currentQuestion = 0;
 let score = 0;
+let timer ;
+let timeLeft;
+
 
 const timerEl = document.getElementById("timer");
 const questionEl = document.getElementById("question");
@@ -48,7 +51,10 @@ const resultEl = document.getElementById("result");
 
 function loadQuestion()
 {
-
+   clearInterval(timer);
+   timeLeft = 15;
+   updateTimer();
+   timer = setInterval(countdown, 1000);
     const q = questions[currentQuestion];
     questionEl.textContent =  `Question ${currentQuestion+1}: ${q.question} `;
     optionsEl.innerHTML = "";
@@ -56,10 +62,7 @@ function loadQuestion()
      const btn = document.createElement("button");
      btn.classList.add("option-btn");
      btn.textContent =option;
-    btn.addEventListener("click", ()=>
-        {
-            selectAnswer(index)
-        });
+    btn.addEventListener("click", ()=>selectAnswer(index, true));
      optionsEl.append(btn);
       
     });
@@ -68,15 +71,29 @@ nextBtn.style.display = "none";
 
 
 }
+function countdown()
+{
+    timeLeft--;
+    updateTimer();
+    if(timeLeft== 0)
+    {
+        clearInterval(timer);
+        selectAnswer(questions[currentQuestion]?.correct, false);
+    }
+}
+function updateTimer()
+{
+    timerEl.textContent = ` ${timeLeft}`
+}
 
-function selectAnswer(index)
+function selectAnswer(index, shouldScore)
 {
 
     const q = questions[currentQuestion];
     const buttons = document.querySelector(".option-btn");
     if(index == q.correct)
     {
-        score++;
+       shouldScore &&  score++;
         buttons[index].classList.add("correct");
     }
     else
@@ -96,15 +113,27 @@ nextBtn.addEventListener("click", ()=>
     }
     else
     {
-
+       showResult();
     }
 })
 
 
-
-
-
-
+function showResult()
+{
+    const highScore = localStorage.getItem("quizHighScore");
+    const isNew = score> highScore;
+    if(isNew)
+    {
+      localStorage.setItem("quizHighScore", score);
+    }
+    resultEl.innerHTML = `
+    <h2> Hurry!! Quiz is finished </h2>
+    <p>You have scored ${score} out of ${questions.length} questions.</P>
+    <p>Highest Score: ${Math.max(score, highScore)} </p>
+    ${isNew ? "<p> Hey, New High Score! </p>": ""}
+    <button onclick = "location.reload()"> Restart Quiz </button>
+    `
+}
 
 loadQuestion();
 
